e743c753f97ef91b2ac185e82600503242a77181
Miklelottesen <miklelottesen@gmail.com>
2015-07-08 15:25:39 +0200

    Mo shizzle

--- resources/views/components/p2_services.php
+++ resources/views/components/p2_services.php
@@ -1,31 +1,56 @@
 <div id="services" class="container">
 	<section class="row text-center">
 		<h2>Shit we do:</h2>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Premium websites</h3>
+				<p>Stand out from your competitors with our tailor made solutions, uniquely designed for flexible, ease of use.</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>App development</h3>
+				<p>If your company needs an app, we will make it. We make apps for Android and iPhones.</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Get discovered</h3>
+				<p>Get on the top of search results. We also help out with sponsored search results.</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Re-design</h3>
+				<p>We will improve what already works on your existing site, and add features to improve it even further.</p>
+			</div>
+		</a>
 	</section>
+	<hr />
 	<section class="row text-center">
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
-		<div class="col-sm-3">
-			<h3>Penis</h3>
-		<p>Some people prefer paragraphs, but at enbit.dk we are more into penises. Vaginas too, but only after 7 liters of vodka.</div>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Print design</h3>
+				<p>With well-designed flyers and business cards, you will be sure that clients will come back to you.</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Online shop</h3>
+				<p>Our tailor-made e-commerce solutions fits any convievable kind of usage. Boost your sales and your order management flows.</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Business solutions</h3>
+				<p>We can create applications for employee-management, account handling and other stuff - whatever you need to make your company run smooth!</p>
+			</div>
+		</a>
+		<a href="#">
+			<div class="col-sm-3">
+				<h3>Support</h3>
+				<p>When we've delivered your product, we will keep in touch to ensure that everything works, and make improvements based on usage statistics.</p>
+			</div>
+		</a>
 	</section>
 </div>
