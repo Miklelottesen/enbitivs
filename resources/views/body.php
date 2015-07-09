51a6cded2fddeb373520eeae7826a9a5149c726f
Miklelottesen <miklelottesen@gmail.com>
2015-07-09 14:12:14 +0200

    Dawg

--- resources/views/body.php
+++ resources/views/body.php
@@ -1,5 +1,6 @@
 <?php include $components.'navbar.php'; ?>
 <?php include $components.'p1_header.php'; ?>
+<?php include $components.'p1_intro.php'; ?>
 <?php include $components.'p2_services.php'; ?>
 <?php include $components.'p3_portfolio.php'; ?>
 <?php include $components.'p4_about.php'; ?>
---
+++ resources/views/components/p1_intro.php
@@ -0,0 +1,3 @@
+<section class="container-fluid row">
+	<p>We do shit, so you can get shit, so you can earn shit. Without shit, is not the very essence of life corrupt? What are we without those brown, tasty delights? Nothing! THAT'S what we do..
+</section>
