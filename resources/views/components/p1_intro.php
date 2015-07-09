f108390f41999493489ddfc35c055e18de722a9b
viliusbivainis <viliusbiv@gmail.com>
2015-07-09 15:43:23 +0200

    hr fun, css naming fun

--- resources/views/components/p1_intro.php
+++ resources/views/components/p1_intro.php
@@ -1,5 +1,7 @@
-<section class="container">
-	<div class="row">
-		<p>We are a web agency located in Aarhus, Denmark. We create web- and app based solutions, tailor-made for our clients to make them stand out, and allow for flexibility.</p>
+<section id="intro" class="container">
+	<div class="row text-center">
+        <h2>Who we are</h2>
+        <hr>
+		<p class="lead">We are a web agency located in Aarhus, Denmark. We create web- and app based solutions, tailor-made for our clients to make them stand out, and allow for flexibility.</p>
 	</div>
 </section>
