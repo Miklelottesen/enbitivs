e5fa9a4ce7d8b8a87b86a3c2089de2d69f4907cb
viliusbivainis <viliusbiv@gmail.com>
2015-07-09 13:23:07 +0200

    iconzzzz

--- resources/views/components/p2_services.php
+++ resources/views/components/p2_services.php
@@ -3,25 +3,28 @@
 		<h2>What we do</h2>
 		<a href="#">
 			<div class="col-sm-3 well">
-                <i class="fa fa-diamond fa-2x"></i>
+                <i class="fa fa-diamond fa-4x"></i>
 				<h3>Premium websites</h3>
 				<p>Stand out from your competitors with our tailor made solutions, uniquely designed for flexible, ease of use.</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-connectdevelop fa-4x"></i>
 				<h3>App development</h3>
 				<p>If your company needs an app, we will make it. We make apps for Android and iPhones.</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-line-chart fa-4x"></i>
 				<h3>Get discovered</h3>
 				<p>Get on the top of search results. We also help out with sponsored search results.</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-medkit fa-4x"></i>
 				<h3>Re-design</h3>
 				<p>We will improve what already works on your existing site, and add features to improve it even further.</p>
 			</div>
@@ -30,24 +33,28 @@
 	<section class="row text-center">
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-print fa-4x"></i>
 				<h3>Print design</h3>
 				<p>With well-designed flyers and business cards, you will be sure that clients will come back to you.</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-shopping-cart fa-4x"></i>
 				<h3>Online shop</h3>
 				<p>Our tailor-made e-commerce solutions fits any convievable kind of usage. Boost your sales and your order management flows.</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-briefcase fa-4x"></i>
 				<h3>Business solutions</h3>
 				<p>We can create applications for employee-management, account handling and other stuff - whatever you need to make your company run smooth!</p>
 			</div>
 		</a>
 		<a href="#">
 			<div class="col-sm-3 well">
+                <i class="fa fa-question-circle fa-4x"></i>
 				<h3>Support</h3>
 				<p>When we've delivered your product, we will keep in touch to ensure that everything works, and make improvements based on usage statistics.</p>
 			</div>
--- resources/views/head.php
+++ resources/views/head.php
@@ -15,6 +15,9 @@
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
 
+<!-- 3rd party transition -->
+<script src="js/bjquery.transit.min.js"></script>
+
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!--[if lt IE 9]>
   <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
