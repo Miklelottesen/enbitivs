4ca57d5ffd0cb6757c0ebf60a0088305af04dd78
Miklelottesen <miklelottesen@gmail.com>
2015-07-13 10:04:48 +0200

    Made string. Yay

---
+++ resources/strings.php
@@ -0,0 +1,62 @@
+<?php
+	// Navbar - en
+	$lang['en']['nav']['home'] = 'Home';
+	$lang['en']['nav']['services'] = 'Services';
+	$lang['en']['nav']['portfolio'] = 'Portfolio';
+	$lang['en']['nav']['about'] = 'About us';
+	$lang['en']['nav']['lang'] = 'Dansk';
+	// Navbar - da
+	$lang['da']['nav']['home'] = 'Forside';
+	$lang['da']['nav']['services'] = 'Produkter';
+	$lang['da']['nav']['portfolio'] = 'Portfolio';
+	$lang['da']['nav']['about'] = 'Om os';
+	$lang['da']['nav']['lang'] = 'English';
+
+	// P1_header/intro - en
+	$lang['en']['p1']['slogan'] = 'Websites, apps and more..';
+	$lang['en']['p1']['introtitle'] = 'Who we are';
+	$lang['en']['p1']['introtext'] = 'We are a web agency located in Aarhus, Denmark. We create web- and app based solutions, tailor-made for our clients to make them stand out, and allow for flexibility.';
+	// P1_header/intro - da
+	$lang['da']['p1']['slogan'] = 'Websites, apps og mere..';
+	$lang['da']['p1']['introtitle'] = 'Hvem er vi?';
+	$lang['da']['p1']['introtext'] = 'Vi er et web bureau i Aarhus. Vi udvikler web- og app-baserede løsninger, skræddersyet til vores klienter, for at de kan skille sig ud med vores simple, flexible produkter.';
+
+	// P2_services - en
+	$lang['en']['p2']['servicestitle'] = 'What we do';
+	$lang['en']['p2']['premiumtitle'] = 'Premium websites';
+	$lang['en']['p2']['premiumtext'] = 'Stand out from your competitors with our tailor-made solutions, uniquely designed for flexible ease of use.';
+	$lang['en']['p2']['apptitle'] = 'App development';
+	$lang['en']['p2']['apptext'] = 'If your company needs an app, we will make it. We make apps for Android and iPhones.';
+	$lang['en']['p2']['seotitle'] = 'Get discovered';
+	$lang['en']['p2']['seotext'] = 'Using search engine optimization, we ensure that your site gets to the top of the search results.';
+	$lang['en']['p2']['redesigntitle'] = 'Re-design';
+	$lang['en']['p2']['redesigntext'] = 'We will improve what already works on your existing site, and add features to improve it even further.';
+	$lang['en']['p2']['printtitle'] = 'Print';
+	$lang['en']['p2']['printtext'] = 'With well-designed flyers and business cards, you will be sure that your clients will come back to you.';
+	$lang['en']['p2']['shoptitle'] = 'Online shop';
+	$lang['en']['p2']['shoptext'] = 'Our tailor-made e-commerce solutions fits any convievable kind of use. Boost your sales and your overview.';
+	$lang['en']['p2']['biztitle'] = 'Business solutions';
+	$lang['en']['p2']['biztext'] = 'We can make applications for employee management, account handling or whatever you need to make your business run smooth.';
+	$lang['en']['p2']['supporttitle'] = 'Support';
+	$lang['en']['p2']['supporttext'] = "When we've delivered our product, we will keep in touch to ensure that everything works, and make imporovements based on usage statistics.";
+	// P2_services - en
+	$lang['en']['p2']['servicestitle'] = 'Hvad laver vi?';
+	$lang['en']['p2']['premiumtitle'] = 'Premium websites';
+	$lang['en']['p2']['premiumtext'] = 'Skil dig ud fra dine konkurrenter med vores skræddersyede løsninger, som er fleksible og designet til at dække dine behov.';
+	$lang['en']['p2']['apptitle'] = 'App-udvikling';
+	$lang['en']['p2']['apptext'] = 'Hvis din virksomhed har brug for en app, laver vi den. Vi udvikler apps til Android of iPhone.';
+	$lang['en']['p2']['seotitle'] = 'Bliv opdaget';
+	$lang['en']['p2']['seotext'] = 'Med <i>search engine optimization</i>, sikrer vi at din website kommer øverst blandt søgeresultaterne.';
+	$lang['en']['p2']['redesigntitle'] = 'Re-design';
+	$lang['en']['p2']['redesigntext'] = 'Hvis du mangler en ny website, laver vi den baseret på hvad der allerede er godt på din nuværende site, og forbedrer hvor der er behov.';
+	$lang['en']['p2']['printtitle'] = 'Tryk';
+	$lang['en']['p2']['printtext'] = 'Med pæne brochurer og visitkort, vil dine kunder vender tilbage til dig.';
+	$lang['en']['p2']['shoptitle'] = 'Web shop';
+	$lang['en']['p2']['shoptext'] = 'Vores skræddersyede e-commerce løsninger passer til alle tænkelige anveldelsesformer. Forøg dit salg og dit overblik.';
+	$lang['en']['p2']['biztitle'] = 'Virksomhedsløsninger';
+	$lang['en']['p2']['biztext'] = 'Vi kan lave applikationer til personale management, regnskabshåndtering eller hvad end din virksomhed har brug for, for at køre flydende.';
+	$lang['en']['p2']['supporttitle'] = 'Support';
+	$lang['en']['p2']['supporttext'] = "Når vi har leveret vores produkt, holder vi kontakten for at sikre os at alt virker optimalt, og for at lave forbedringer baseret på brugsstatistikker.";
+
+	
+?>
