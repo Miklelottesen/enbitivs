28088fdbd817a7f38c02c6dcccf38ed30ba39919
viliusbivainis <viliusbiv@gmail.com>
2015-07-13 10:35:20 +0200

    aboutus

--- resources/views/components/p1_intro.php
+++ resources/views/components/p1_intro.php
@@ -5,7 +5,7 @@
 		<p class="lead"><?php echo $lang[$la]['p1']['introtext'];?></p>
 	</div>
 </section>
-        <div id="TriangleContainer">
+        <div class="TriangleContainer">
             <svg id="TriangleShadow" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
                 <path id="WhiteTrianglePath1" d="M0 0 L50 100 L100 0 Z"></path>
                 <path id="WhiteTrianglePath2" d="M50 100 L100 40 L100 0 Z"></path>
---
+++ resources/views/components/p4_about.php
@@ -0,0 +1,13 @@
+<div id="about_us" class="container">
+    <div class="row text-center">
+        <h2><?php echo $lang[$la]['p1']['introtitle'];?></h2>
+        <hr>
+        <p class="lead"><?php echo $lang[$la]['p1']['introtext'];?></p>
+    </div>
+</div>
+<div class="TriangleContainer">
+    <svg id="BlackTriangleShadow" xmlns="http://www.w3.org/2000/svg" version="1.1" width="100%" height="100" viewBox="0 0 100 100" preserveAspectRatio="none">
+        <path id="WhiteTrianglePath1" d="M0 0 L50 100 L100 0 Z"></path>
+        <path id="WhiteTrianglePath2" d="M50 100 L100 40 L100 0 Z"></path>
+    </svg>
+</div>
