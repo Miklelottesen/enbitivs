f55518f9ddb25d9dcf3d7eb95a1167e44046bae4
Miklelottesen <miklelottesen@gmail.com>
2015-07-14 13:27:26 +0200

    Mailshit

---
+++ resources/sendmail.php
@@ -0,0 +1,41 @@
+<?php
+	session_start();
+	$returnSuccess = false; // Will change to true if mail is successfully sent
+	if(!isset($_SESSION['last_post']) || $_SESSION['last_post'] + 10 < time()) {
+		if (isset($_GET['email']) && isset($_GET['name']) && isset($_GET['message'])) {
+			// If mail, name and message are filled
+			$name = base64_decode($_GET['name']);
+			$email = base64_decode($_GET['email']);
+			$message = base64_decode($_GET['message']);
+			$callme = $_GET['callme'];
+
+			if (!preg_match( "/[\r\n]/", $name ) && !preg_match("/[\r\n]/", $email) && !filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
+				// If no injections were found and email is a valid email
+				$message = '<p>'.$message.'</p>';
+				if($callme) {
+					$message = '<p><strong>This user wishes to be called</strong></p>'.$message;
+				}
+
+				$message = '
+				<html> 
+				  <body> 
+				  '.$message.'
+				  </body> 
+				</html> 
+				';
+
+				$_SESSION['last_post'] = time();
+				$returnSuccess = true;
+				$headers  = 'MIME-Version: 1.0' . "\r\n";
+				$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+				$headers .= 'From: '.$name.''."\r\n".
+				'Reply-To: '.$email.''."\r\n".
+				'X-Mailer: PHP/'.phpversion();
+
+				mail('info@enbit.dk', 'Mail from webform', $message, $headers);
+			} 
+		}
+	}
+	echo json_encode($returnSuccess);
+	die();
+?>
