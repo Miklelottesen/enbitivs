ee561e621718c75b6a7e886985a6f3aa546005dd
viliusbivainis <viliusbiv@gmail.com>
2015-07-16 15:06:38 +0200

    typog

--- resources/views/components/p1_intro.php
+++ resources/views/components/p1_intro.php
@@ -5,7 +5,7 @@
 		<p class="lead"><?php echo $lang[$la]['p1']['introtext'];?></p>
         <div class="clearfix"></div>
         <hr class="intro_hr">
-        <p class="lead"><?php echo $lang[$la]['p1']['introcall'];?></p>
+        <p class="lead actionp"><?php echo $lang[$la]['p1']['introcall'];?></p>
         <button id="calltoaction" class="btn btn-5 btn-5a glyphicon glyphicon-menu-down"><span><?php echo $lang[$la]['p1']['introbutton'];?></span></button>
 	</div>
 </section>
