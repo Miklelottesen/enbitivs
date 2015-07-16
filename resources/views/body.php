c53775f2c1ceae6c2f63075173c78b3d27f625b4
Miklelottesen <miklelottesen@gmail.com>
2015-07-16 10:15:06 +0200

    Facebook error fix + static height for 'services'

--- resources/views/body.php
+++ resources/views/body.php
@@ -74,6 +74,6 @@
         var js, fjs = d.getElementsByTagName(s)[0];
         if (d.getElementById(id)) return;
         js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v2.4&appId=1633550523587502";
+        js.src = "//connect.facebook.net/en_GB/sdk.js";
         fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
