bca0590a585ed7da639341d92671a7937e3ece57
Miklelottesen <miklelottesen@gmail.com>
2015-07-17 14:46:49 +0200

    Dem strings

--- resources/views/articles/strings.php
+++ resources/views/articles/strings.php
@@ -9,7 +9,11 @@
 <p>This approach allows for flexibility, and it will benefit your users as well as yourself - with a customized content management system, we can make sure that you will be able to alter any aspect of the site that you desire, through a simple and efficient user-interface. Thus, you will save the time and resources you need to run your company.</p>
 
 <p>At enbit.dk, we will meet your needs and standards, whether you need a blog, a web shop, a promotional microsite or just a simple one-page website with contact info. With beautiful design and intuitive interfaces, we make sure that your clients and customers will choose your company over your competitors.</p>';
-	$article_lang['da']['premium'] = '<p>Many agencies will provide you with a quick, sparsely customized website using standardized frameworks. At enbit.dk, we wish to provide you with the best possible solutions. We achieve that by going in depth with the design and coding, and applying the necessary server- and client-side scripts to meet your needs.<p>This approach allows for flexibility, and it will benefit your users as well as yourself - with a customized content management system, we can make sure that you will be able to alter any aspect of the site that you desire, through a simple and efficient user-interface. Thus, you will save the time and resources you need to run your company.<p>At enbit.dk, we will meet your needs and standards, whether you need a blog, a web shop, a promotional microsite or just a simple one-page website with contact info. With beautiful design and intuitive interfaces, we make sure that your clients and customers will choose your company over your competitors.</p>';
+	$article_lang['da']['premium'] = '<p>Mange webdesign firmaer vil give dig en hurtig, minimalt tilpasset hjemmeside ved at bruge standardiserede frameworks. Hos enbit.dk, ønsker vi at give dig de bedst mulige løsninger. Det opnår vi ved at gå i dybden med design og kodning, og ved at anvende de nødvendige server-og klient-scripts til at opfylde dine behov.</p>
+
+<p>Denne fremgangsmåde giver mulighed for fleksibilitet, og det gavner dine brugere såvel som dig selv - med et tilpasset content management system, kan vi sørge for, at du vil være i stand til at ændre ethvert ønsket aspekt af din website, ved hjælp af en enkel og effektiv brugerflade. Således vil du spare den tid og de ressourcer, du har brug for til at drive din virksomhed.</p>
+
+<p>Hos enbit.dk, vil vi opfylde dine behov og standarder, uanset om du har brug for en blog, en webshop, en promovativ microsite eller bare en simpel enkelt-side website med kontakt info. Med smukt design og intuitive grænseflader, sørger vi for, at dine kunder vil vælge din virksomhed fremfor dine konkurrenter.</p>';
 
 
 	// App
@@ -18,29 +22,48 @@
 <p>We can make apps for Android, iPhone and Windows phones and we will assist you with getting the app onto the various platforms’ marketplaces and make sure that the app appears with the top search results. If necessary, for a small fee, we will keep working on the app after it launches, to implement updates and make sure that it runs smoothly on all targeted devices.</p>
 
 <p>When we design apps, we put a great importance in designing it for the targeted device. Android apps has one design, iOS apps has another and so on. This will make the app intuitive to the user, because it will look familiar.</p>';
-	$article_lang['da']['app'] = '<p>Appy solutions</p>';
+	$article_lang['da']['app'] = '<p>I disse dage er apps til mobile enheder enormt populære. Mange virksomheder bruger apps til forskellige formål. Hos enbit.dk, vil vi hjælpe dig med at afgøre, om du har brug for en app, samt hvilken type app du har brug for, gratis. Derefter vil vi udvikle appen, tilpasset til dine behov.</p>
+
+<p>Vi kan lave apps til Android, iPhone og Windows-telefoner, og vi vil hjælpe dig med at få appen ud på de forskellige platformes markedspladser, og sørge for, at appen vises blandt de øverste søgeresultater. Hvis det er nødvendigt vil vi, for et mindre gebyr, blive ved med at arbejde på appen, efter at den er udgivet, for at udføre opdateringer, og sørge for, at den kører problemfrit på alle understøttede enheder.</p>
+
+<p>Når vi designer apps, lægger vi stor vægt på at designe dem for de specifikke enheder. Android apps har ét design, iOS apps har et andet, og så videre. Dette vil gøre appen intuitiv for brugeren, fordi designet vil virke bekendt.</p>';
 
 	// SEO
-	$article_lang['en']['seo'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['seo'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['seo'] = '<p>Having a website is only important if your users are able to find it. Through search engine optimization, we can get your website among the top search results on all the big search engines, such as Google, Yahoo and Bing. A huge part of search engine optimization relies on the content, so we will make sure to help you with your content so you deliver the right message, in a search engine friendly manner.</p>
+
+<p>In the process, we will make sure to adequately include social media on your website, which will exponentially increase your chance of being discovered. If you need it, we will also help out with making your website part of the sponsored search results.</p>';
+	$article_lang['da']['seo'] = '<p>At have en hjemmeside er kun en fordel, hvis dine brugere er i stand til at finde den. Gennem search engine optimization, kan vi få din hjemmeside blandt de øverste søgeresultater på alle de store søgemaskiner såsom Google, Yahoo og Bing. En stor del af søgemaskineoptimering afhænger af sidens indhold, så vi vil sørge for at hjælpe dig med dit indhold, så du leverer det rette budskab, på en søgemaskine-venlig måde.</p>
+
+<p>I processen vil vi sørge for at inkludere sociale medier på din hjemmeside, som eksponentielt vil øge din chance for at blive opdaget. Hvis du har brug for det, vil vi også hjælpe med at gøre din hjemmeside en del af de sponsorerede søgeresultater.</p>';
 
 	// Re-design
-	$article_lang['en']['redesign'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['redesign'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['redesign'] = '<p>If your current website needs a facelift, we will help you out. First, we will analyze your current site to find out what already works well, then we will implement those aspects into a brand new website that improves on what your old site lacks.
+If you need it, we will help you with re-branding, but if not we will be faithful to your current brand when redesigning your website. As with our premium website solution, our goal is to deliver a tailor-made product, that suits your needs.</p>';
+	$article_lang['da']['redesign'] = '<p>Hvis din nuværende hjemmeside har brug for en ansigtsløftning, vil vi hjælpe dig. Først vil vi analysere din nuværende hjemmeside, for at finde ud af, hvad der allerede fungerer godt. Derefter vil vi implementere disse aspekter i en helt ny hjemmeside, der forbedrer din gamle hjemmesides mangler.</p>
+
+<p>Hvis du har brug for det, vil vi hjælpe dig med re-branding, men hvis ikke, vil vi være tro mod dit nuværende brand, når vi om-designer din hjemmeside. Som med vores premium website løsning, er vores mål at levere et skræddersyet produkt, der passer til dine behov.</p>';
 
 	// Print
-	$article_lang['en']['print'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['print'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['print'] = '<p>If you need posters, flyers or business cards, we will design them for you and get them printed and delivered at a low cost. Attract potential and current clients and customers with beautifully designed prints, and make them go back to you with outstanding business cards. At enbit.dk, our goal is to deliver the quality you need.</p>';
+	$article_lang['da']['print'] = '<p>Hvis du har brug for plakater, flyers eller visitkort, vil vi designe dem for dig og få dem trykt og leveret til en lav pris. Tiltræk potentielle og nuværende kunder med smukt designede tryksager, og få dem til at vende tilbage til dig, med exceptionelle visitkort. Hos enbit.dk, er vores mål at levere dén kvalitet, du har brug for.</p>';
 
 	// Shop
-	$article_lang['en']['shop'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['shop'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['shop'] = '<p>As with our premium website solutions, we put a great effort into providing you with a tailor-made ecommerce solution. We discard the ready-made templates, in order to make a web shop that is customized to meet your needs, and suit your range of products.</p>
+
+<p>We will make sure that the webshop is easy to manage, whilst being intuitive for the user. This will attract more customers, and provide you with more time and resources to run your business.</p>';
+	$article_lang['da']['shop'] = '<p>Som med vores premium website løsninger, lægger vi en stor indsats i at give dig en skræddersyet ecommerce løsning. Vi kasserer de færdiglavede skabeloner, for at lave en web butik, der er skræddersyet til at opfylde dine behov, og som passer til dit produktsortiment.</p>
+
+<p>Vi vil sørge for, at webshoppen er nem at administrere, og samtidig er intuitiv for brugeren. Dette vil tiltrække flere kunder, og give dig mere tid og ressourcer til at drive din forretning.</p>';
 
 	// Business
-	$article_lang['en']['biz'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['biz'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['biz'] = '<p>If you need an application to manage the internal aspects of your company, we will make it for you. We will do the codework and design it so that it is easy to use. Our goal is to minimize the efforts on your part, whether you need an employee management application, an account management system, an internal communication system or whatever your company needs.</p>
+
+<p>Security is one of our top concerns, and we will code the application to use secure, encrypted data that you need to keep classified. We will not have any access to your data ourselves.</p>';
+	$article_lang['da']['biz'] = '<p>Hvis du har brug for en applikation til at håndtere de interne aspekter af din virksomhed, vil vi lave den for dig. Vi vil lave kode arbejdet og designe den, så den er let at bruge. Vores mål er at minimere indsatsen fra din side, uanset om du har brug for et medarbejder-management program, et rengskabshåndteringssystem, et internt kommunikationssystem eller hvad end din virksomhed har brug for.</p>
+
+<p>Sikkerhed er en af ​​vores største prioriteter, og vi vil kode applikationen til at anvende sikker, krypteret data, på det indhold som du har brug for at holde klassificeret. Vi vil ikke have nogen adgang til dine data.</p>';
 
 	// Support
-	$article_lang['en']['support'] = '<p>Shitty solutions</p>';
-	$article_lang['da']['support'] = '<p>Shitty solutions</p>';
+	$article_lang['en']['support'] = '<p>When we have delivered our product to you, we will keep in touch for a small fee, to ensure that everything works, to fix whatever errors might occur and make sure that you and your staff knows how to use it. We implement analytics tools in our products, so that we can track all user interactions and make improvements where it’s needed.</p>';
+	$article_lang['da']['support'] = '<p>Når vi har leveret vores produkt til dig, vil vi holde kontakten for et mindre gebyr, for at sikre, at alt fungerer optimalt, for at reparere eventuelle fejl der kan opstå og for sørge for, at du og dine medarbejdere ved, hvordan man anvender det. Vi implementerer analyseværktøjer i vores produkter, så vi kan spore alle brugerinteraktioner og foretage forbedringer, der hvor det er nødvendigt.</p>';
 ?>
