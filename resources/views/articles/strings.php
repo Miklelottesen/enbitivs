8029cf9af0a4923288b86e4eb6f55d1c5a1d0a41
Miklelottesen <miklelottesen@gmail.com>
2015-07-17 13:54:26 +0200

    String theories

--- resources/views/articles/strings.php
+++ resources/views/articles/strings.php
@@ -5,8 +5,8 @@
 
 	// Premium
 	$article_lang['en']['premium'] = '<p>While many agencies merely piece together peices of standard templates, we go around that and deliver tailor-made products to our clients. This allows you to get a flexible solution, based on your needs, and provides your users with the best possible experience.</p><p>We do not only develop for your users, we also develop for you - custom content management systems with user friendly interfaces enables you do edit and manage your site as seamlessly as possible, on any aspect of your choice.</p>';
-	$article_lang['da']['premium'] = '<p>While many agencies merely piece together peices of standard templates, we go around that and deliver tailor-made products to our clients. This allows you to get a flexible solution, based on your needs, and provides your users with the best possible experience.</p><p>We do not only develop for your users, we also develop for you - custom content management systems with user friendly interfaces enables you do edit and manage your site as seamlessly as possible, on any aspect of your choice.</p>';
-	
+	$article_lang['da']['premium'] = '<p>Many agencies will provide you with a quick, sparsely customized website using standardized frameworks. At enbit.dk, we wish to provide you with the best possible solutions. We achieve that by going in depth with the design and coding, and applying the necessary server- and client-side scripts to meet your needs.<p>This approach allows for flexibility, and it will benefit your users as well as yourself - with a customized content management system, we can make sure that you will be able to alter any aspect of the site that you desire, through a simple and efficient user-interface. Thus, you will save the time and resources you need to run your company.<p>At enbit.dk, we will meet your needs and standards, whether you need a blog, a web shop, a promotional microsite or just a simple one-page website with contact info. With beautiful design and intuitive interfaces, we make sure that your clients and customers will choose your company over your competitors.</p>';
+
 
 	// App
 	$article_lang['en']['app'] = '<p>Appy solutions</p>';
