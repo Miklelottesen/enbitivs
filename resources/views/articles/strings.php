0ddf1d77da69a56de271a2e516cfa6e4c08d491f
Miklelottesen <miklelottesen@gmail.com>
2015-07-17 14:24:22 +0200

    Some strings attached

--- resources/views/articles/strings.php
+++ resources/views/articles/strings.php
@@ -4,12 +4,20 @@
 	$p2Error['da'] = 'Beklager, der opstod en fejl.';
 
 	// Premium
-	$article_lang['en']['premium'] = '<p>While many agencies merely piece together peices of standard templates, we go around that and deliver tailor-made products to our clients. This allows you to get a flexible solution, based on your needs, and provides your users with the best possible experience.</p><p>We do not only develop for your users, we also develop for you - custom content management systems with user friendly interfaces enables you do edit and manage your site as seamlessly as possible, on any aspect of your choice.</p>';
+	$article_lang['en']['premium'] = '<p>Many agencies will provide you with a quick, sparsely customized website using standardized frameworks. At enbit.dk, we wish to provide you with the best possible solutions. We achieve that by going in depth with the design and coding, and applying the necessary server- and client-side scripts to meet your needs.</p>
+
+<p>This approach allows for flexibility, and it will benefit your users as well as yourself - with a customized content management system, we can make sure that you will be able to alter any aspect of the site that you desire, through a simple and efficient user-interface. Thus, you will save the time and resources you need to run your company.</p>
+
+<p>At enbit.dk, we will meet your needs and standards, whether you need a blog, a web shop, a promotional microsite or just a simple one-page website with contact info. With beautiful design and intuitive interfaces, we make sure that your clients and customers will choose your company over your competitors.</p>';
 	$article_lang['da']['premium'] = '<p>Many agencies will provide you with a quick, sparsely customized website using standardized frameworks. At enbit.dk, we wish to provide you with the best possible solutions. We achieve that by going in depth with the design and coding, and applying the necessary server- and client-side scripts to meet your needs.<p>This approach allows for flexibility, and it will benefit your users as well as yourself - with a customized content management system, we can make sure that you will be able to alter any aspect of the site that you desire, through a simple and efficient user-interface. Thus, you will save the time and resources you need to run your company.<p>At enbit.dk, we will meet your needs and standards, whether you need a blog, a web shop, a promotional microsite or just a simple one-page website with contact info. With beautiful design and intuitive interfaces, we make sure that your clients and customers will choose your company over your competitors.</p>';
 
 
 	// App
-	$article_lang['en']['app'] = '<p>Appy solutions</p>';
+	$article_lang['en']['app'] = '<p>In these days, apps for mobile devices are hugely popular. Many companies use apps for various reasons. At enbit.dk, we will help you with determining if you need an app, and what type of app you’d need, for free. Then we’ll make the app, customized to meet your needs.</p>
+
+<p>We can make apps for Android, iPhone and Windows phones and we will assist you with getting the app onto the various platforms’ marketplaces and make sure that the app appears with the top search results. If necessary, for a small fee, we will keep working on the app after it launches, to implement updates and make sure that it runs smoothly on all targeted devices.</p>
+
+<p>When we design apps, we put a great importance in designing it for the targeted device. Android apps has one design, iOS apps has another and so on. This will make the app intuitive to the user, because it will look familiar.</p>';
 	$article_lang['da']['app'] = '<p>Appy solutions</p>';
 
 	// SEO
