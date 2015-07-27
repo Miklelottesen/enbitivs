01b8a2c7e570c7b127b53599e03be6283a8c4029
Miklelottesen <miklelottesen@gmail.com>
2015-07-27 15:23:38 +0200

    Added sr-only h1

--- resources/views/components/p1_header.php
+++ resources/views/components/p1_header.php
@@ -1,5 +1,6 @@
 <div id="introHeader" class="container-fluid arrow_box">
 	<div class="row">
+		<h1 class="sr-only">enbit.dk</h1>
 		<h2><?php echo $lang[$la]['p1']['slogan'];?></h2>
 	</div>
 </div>
