6c95e407b20c3393824c675c1e0b4e5a0ccbf26f
viliusbivainis <vilius@enbit.dk>
2015-07-27 11:56:41 +0200

    boop

--- resources/views/head.php
+++ resources/views/head.php
@@ -17,7 +17,7 @@
 <script src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <!-- Include all compiled plugins (below), or include individual files as needed -->
 <script src="js/bootstrap.min.js"></script>
-<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300' rel='stylesheet' type='text/css'>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
 
 <!-- ScrollTo script -->
 <script src="js/jquery-scrollto.js"></script>
