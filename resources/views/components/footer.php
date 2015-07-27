eaab1da4a88f11707bbeb3d1da66df3209ac4660
viliusbivainis <vilius@enbit.dk>
2015-07-27 14:24:33 +0200

    fb shit

--- resources/views/components/footer.php
+++ resources/views/components/footer.php
@@ -3,17 +3,9 @@
         <div id="footerlogo" class="row text-center">
 
                     <img src="img/logo_footer.png" class="img-resposive" alt="footer logo">
-            <div id="fbContainer" class="col-xs-offset-2">
-                <div class="fb-like"
-                     data-href="https://www.facebook.com/enbitdk"
-                     data-layout="standard"
-                     data-action="like"
-                     data-show-faces="false"
-                     data-share="true"
-                     data-adapt-container-width="true">
-                </div>
 
-            </div>
+
+
         </div>
 
 
